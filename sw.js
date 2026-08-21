6ba317ba80b614819ad64df17d037e3040d31313
jaredfowkes <jaredfowkes@me.com>
2026-08-21 13:20:37 -0700

    Sync sw.js from workbench

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
 
    Escape hatch: load any page with ?nosw=1 to unregister and drop all caches.
 */
-const VERSION = '2026-08-21.26';
+const VERSION = '2026-08-20.22';
 const CACHE = 'workbench-shell-' + VERSION;
 const SHELL = ['./', './index.html', './manifest.webmanifest'];
 
